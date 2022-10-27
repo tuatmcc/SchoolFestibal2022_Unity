578c8acda904bc56fab93802e3621b53ad4c2c32
ojiichan <84656786+OJII3@users.noreply.github.com>
2022-10-27 23:28:59 +0900

    カメラワークをRaceシーンに適用、想定通りの動きではない

--- Assets/RaceGame/Scripts/Race/Misc/CinemachineController.cs
+++ Assets/RaceGame/Scripts/Race/Misc/CinemachineController.cs
@@ -1,6 +1,9 @@
+using System;
+using System.Collections.Generic;
 using Cinemachine;
 using RaceGame.Race.Interface;
 using UnityEngine;
+using UnityEngine.Serialization;
 using Zenject;
 
 namespace RaceGame.Race.Misc
@@ -8,24 +11,54 @@ namespace RaceGame.Race.Misc
     [RequireComponent(typeof(CinemachineVirtualCamera))]
     public class CinemachineController : MonoBehaviour
     {
+        [SerializeField] private CinemachineTargetGroup _targetGroup;
         private CinemachineVirtualCamera _virtualCamera;
-        
+
         [Inject] private IRaceManager _raceManager;
-        
+
         private void Start()
         {
             _virtualCamera = GetComponent<CinemachineVirtualCamera>();
         }
 
+
         private void Update()
         {
+            AddTargets();
             var localPlayer = _raceManager.LocalPlayer;
             if (localPlayer == null) return;
 
-            var localPlayerTransform = localPlayer.transform;
+            // var localPlayerTransform = localPlayer.transform;
+            //
+            // _virtualCamera.Follow = localPlayerTransform;
+            // _virtualCamera.LookAt = localPlayerTransform;
+        }
 
-            _virtualCamera.Follow = localPlayerTransform;
-            _virtualCamera.LookAt = localPlayerTransform;
+        /// <summary>
+        /// CinemachineTargetGroupにプレイヤーを追加
+        /// </summary>
+        private void AddTargets()
+        {
+            // Start内だと早すぎる模様
+            if (_targetGroup.m_Targets.Length < _raceManager.Players.Count)
+            {
+                var playerWeight = 1f;
+                var localPlayerWeight = 4f;
+                var playerRadius = 3f;
+                var targets = new List<CinemachineTargetGroup.Target>();
+                foreach (var player in _raceManager.Players)
+                {
+                    var target = new CinemachineTargetGroup.Target();
+                    target.target = player.transform;
+                    // localPlayerのウェイトだけ重くしようとしたが、判別できていない？
+                    target.weight = (player == _raceManager.LocalPlayer) ? localPlayerWeight : playerWeight;
+                    target.radius = playerRadius;
+                    targets.Add(target);
+                }
+
+                _targetGroup.m_Targets = targets.ToArray();
+                _virtualCamera.LookAt = _targetGroup.transform;
+            }
         }
     }
 }
