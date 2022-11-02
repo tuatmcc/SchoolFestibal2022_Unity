a258049a95b5e4a8f1bba0304f889b1822fba06f
kitashimauni <kitashima.daiki123@gmail.com>
2022-11-02 23:00:58 +0900

    SEを少し追加

--- Assets/RaceGame/Scripts/Race/Sound/BGMManager.cs
+++ Assets/RaceGame/Scripts/Race/Sound/BGMManager.cs
@@ -9,8 +9,11 @@ namespace RaceGame.Race.Sounds
         private RaceManager _raceManager;
         [SerializeField] private GameObject _target;
         [SerializeField] private AudioClip _bgmClip;
+        [SerializeField] private AudioClip _startSeClip;
         private AudioSource _audioSource;
+        private bool _startedBGM = false;
         private bool _fadeOut = false;
+        //private bool _localPlayerFinished = false;
         void Start()
         {
             _raceManager = GetComponent<RaceManager>();
@@ -19,9 +22,11 @@ namespace RaceGame.Race.Sounds
 
         void Update()
         {
-            if( _raceManager.RaceState == RaceState.Racing && !_audioSource.isPlaying )
+            if( _raceManager.RaceState == RaceState.Racing && !_startedBGM)
             {
-                _audioSource.PlayOneShot(_bgmClip);
+                _audioSource.PlayOneShot(_startSeClip);
+                StartCoroutine(StartBGMAfterSconds(_audioSource, _bgmClip, 0.5f));
+                _startedBGM = true;
             }
             if( _raceManager.RaceState == RaceState.Finished && !_fadeOut )
             {
@@ -37,6 +42,11 @@ namespace RaceGame.Race.Sounds
                 yield return new WaitForSeconds(0.1f);
             }
             audioSource.Stop();
+        }
+        private IEnumerator StartBGMAfterSconds(AudioSource audioSource, AudioClip audioClip, float seconds)
+        {
+            yield return new WaitForSeconds(seconds);
+            audioSource.PlayOneShot(audioClip);
         }
     }
 }
