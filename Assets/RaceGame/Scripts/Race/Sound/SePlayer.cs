f5d177b7a69524ec1d350706a3af199a36e3689e
kitashimauni <kitashima.daiki123@gmail.com>
2022-11-04 23:29:32 +0900

    微修正

--- Assets/RaceGame/Scripts/Race/Sound/SePlayer.cs
+++ Assets/RaceGame/Scripts/Race/Sound/SePlayer.cs
@@ -13,6 +13,7 @@ namespace RaceGame.Race.Sounds
 
         private void Start()
         {
+            _prevPosition = transform.position;
             _audioSource = GetComponent<AudioSource>();
             _raceManager = FindObjectOfType<RaceManager>();
         }
