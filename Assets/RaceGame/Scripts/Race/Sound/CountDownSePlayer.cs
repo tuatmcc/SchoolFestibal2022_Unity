5f1b6a00d7d5a71e64db39cde0776785ca06bcbc
kitashimauni <kitashima.daiki123@gmail.com>
2022-11-08 23:09:28 +0900

    タイトルのBGM追加

--- Assets/RaceGame/Scripts/Race/Sound/CountDownSePlayer.cs
+++ Assets/RaceGame/Scripts/Race/Sound/CountDownSePlayer.cs
@@ -4,14 +4,9 @@ using UnityEngine;
 
 public class CountDownSePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    [SerializeField] AudioSource m_AudioSource;
+    [SerializeField] AudioClip audioClip;
+    void StartTitleBGM()
     {
         
     }
--- Assets/RaceGame/Scripts/Title/UI/TitlePageManager.cs
+++ Assets/RaceGame/Scripts/Title/UI/TitlePageManager.cs
@@ -1,5 +1,6 @@
 using RaceGame.Core.Interface;
 using RaceGame.Title.UI.Pages;
+using RaceGame.Title.Sound;
 using UnityEngine;
 using Zenject;
 
@@ -15,11 +16,14 @@ namespace RaceGame.Title.UI
         [SerializeField] private LogoPage logoPage;
         [SerializeField] private QRCodeReaderPage qrCodeReaderPage;
         [SerializeField] private GameModeSelectPage gameModeSelectPage;
+        private TitleBGMPlayer titleBGMPlayer;
 
         [Inject] private IGameSetting _gameSetting;
         
         private void Start()
         {
+            titleBGMPlayer = GetComponent<TitleBGMPlayer>();
+
             _gameSetting.StartFromTitle = true;
 
             SetActivePages(false, true, false);
@@ -29,6 +33,8 @@ namespace RaceGame.Title.UI
             }
 
             qrCodeReaderPage.OnReadQRCode += OnReadQRCode;
+
+            titleBGMPlayer.PlayTitleBGM();
         }
 
         private void OnReadQRCode(int result)
