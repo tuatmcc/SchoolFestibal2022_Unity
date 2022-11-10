ba211da6425ffb80bfbd2b30f3169b838dcf62ea
mkc1370 <mkc1370go@gmail.com>
2022-11-10 21:54:20 +0900

    初めからに戻れるように

--- Assets/RaceGame/Scripts/Race/UI/Page/ResultPage.cs
+++ Assets/RaceGame/Scripts/Race/UI/Page/ResultPage.cs
@@ -1,5 +1,8 @@
+using Mirror;
 using RaceGame.Core.UI;
+using RaceGame.Race.Interface;
 using UnityEngine;
+using Zenject;
 
 namespace RaceGame.Race.UI.Page
 {
@@ -10,7 +13,31 @@ namespace RaceGame.Race.UI.Page
     {
         [SerializeField] private SelectableButton retryButton;
         [SerializeField] private SelectableButton backToTitleButton;
-        
+
+        [Inject] private IRaceManager _raceManager;
+
+        private void Start()
+        {
+            retryButton.OnClicked += Retry;
+            backToTitleButton.OnClicked += BackToTitle;
+        }
+
+        private void BackToTitle()
+        {
+            if (_raceManager.LocalPlayer.isServer)
+            {
+                NetworkManager.singleton.StopHost();
+            }
+            else
+            {
+                NetworkManager.singleton.StopClient();
+            }
+        }
+
+        private void Retry()
+        {
+        }
+
         public void SetActive(bool value)
         {
             gameObject.SetActive(value);
